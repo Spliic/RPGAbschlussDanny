8865d89c4b869fa110204a5128308630891e4419
dannywilliams <dakewi@hotmail.de>
2023-05-04 16:11:30 +0200

    Stand 04.05.2034

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,48 @@
-import helden.Bogenschütze
-import helden.Krieger
-import helden.Magier
+import Kunde.Kundenaccount
+import Produkte.Menü
+import haendler.Haendleraccount
 
+var CYAN = "\u001B[36m"
+var RESET = "\u001B[0m"
 
 fun main() {
 
-
-    var magier = Magier("Kael",100)
-    var krieger = Krieger("Barbar",100)
-    var bogenschuetze = Bogenschütze("Pikantenuss",100)
-
-    for (spell in magier.spells){
-        println(spell.name)
-    }
-    println()
-    for (spell in krieger.spells){
-        println(spell.name)
-    }
-    println()
-
-    val attack = bogenschuetze.spells.random()
-    println("Der Bogenschütze greift mit ${attack.name} an und macht ${attack.strengh} schaden")
+    menuFromStore()
 
 
 }
+
+fun menuFromStore(){
+    var userLoggedIn = false
+    val kundenaccount = Kundenaccount()
+    val haendleraccount = Haendleraccount()
+    val loginUndLogoutUser = LoginUndLogout()
+    val loginUndLogoutHaendler = LoginUndLogout()
+    val menueUser = Menü()
+
+
+    println("""
+            Was möchten sie hier tun?
+            [1] Neues Kundenkonto erstellen
+            [2] Neues Händlerkonto erstellen
+            [3] Kunden-Anmeldung
+            [4] Händler-Anmeldung
+            Bitte Wählen sie eine Zahl von 1-4 (Mit Enter bestätigen)
+        """.trimIndent())
+    var userInputMenu = readln().toInt()
+    when(userInputMenu){
+        1 -> kundenaccount.newAccountForUser()
+        2 -> haendleraccount.newAccountForHaendler()
+        3 -> userLoggedIn = loginUndLogoutUser.logInMenueUser()
+        4 -> loginUndLogoutHaendler.logInMenueHaendler()
+    }
+    println()
+
+    // Wenn Kunde eingeloggt ist, wird Menü für den User geöffnet
+    if (userLoggedIn){
+        menueUser.menueUser()
+    }
+
+}
+
+
---
+++ src/Produkte/Produktbewertung.kt
@@ -0,0 +1,6 @@
+package Produkte
+
+open class Produktbewertung(var name: String, var bewertung: Int, var kommentar: String) {
+
+
+}
